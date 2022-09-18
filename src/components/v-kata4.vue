b3dc889032d3d7406bf9b635b78403c6f1229169
Levin_Ihor <lytghzys@gmail.com>
2022-09-18 23:25:27 +0300

    Day 5

--- src/components/v-kata4.vue
+++ src/components/v-kata4.vue
@@ -35,7 +35,55 @@
 //   return array.reduce((a, b) => a + b, 0);
 // }
 
-solution(10);
+// solution(10);
+
+// function firstNonConsecutive(arr: number[]) {
+//   for (let index = 0; index < arr.length - 1; index++) {
+//     if (arr[index] + 1 !== arr[index + 1]) {
+//       return arr[index + 1];
+//     }
+//   }
+// }
+// console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]));
+
+// function getLengthOfMissingArray(arrayOfArrays: any[]) {
+//   let sortedArrayArrays = arrayOfArrays.sort((a, b) => a.length - b.length);
+//   let temp = 0;
+//   let answer = 0;
+//   console.log(sortedArrayArrays);
+//   if (sortedArrayArrays.length == 0 || sortedArrayArrays[0].length == 0) {
+//     return 0;
+//   }
+//   sortedArrayArrays.forEach((element) => {
+//     if (temp + 1 != element.length) {
+//       answer = temp + 1;
+//     }
+//     temp = element.length;
+//   });
+//   return answer;
+// }
+// console.log(getLengthOfMissingArray([]));
+
+// const towerBuilder = (nFloors: number) => {
+//   let arr = [];
+//   let padding = nFloors - 1;
+//   for (let index = 0; index < nFloors; index++) {
+//     let _arr = "";
+//     for (let j = 1; j < nFloors * 2; j++) {
+//       if (j > padding && j < nFloors * 2 - padding) {
+//         console.log(j > padding, j < nFloors * 2 - padding);
+//         _arr += "*";
+//       } else {
+//         _arr += "";
+//       }
+//     }
+//     padding--;
+//     arr.push(_arr);
+//   }
+
+//   return arr;
+// };
+// console.log(towerBuilder(3));
 </script>
 <template>
   <h1>Kata 4</h1>

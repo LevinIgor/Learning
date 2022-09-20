7d977f7e1201b52b44032cad0c1956b1613d74fa
Levin_Ihor <lytghzys@gmail.com>
2022-09-20 23:03:09 +0300

    Day 7

---
+++ src/components/v-kata5.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+// function index(array: number[], n: number): number {
+//   return array.length >= n ? array[n] ** n : -1;
+// }
+// console.log(index([1, 3, 10, 100], 3));
+
+// function flyBy(lamps: string, drone: string) {
+//   let str = drone
+//     .split("")
+//     .map((l) => l.replace(l, "o"))
+//     .join("");
+//   let sub = lamps.substring(drone.length);
+//   return str + sub;
+// }
+// console.log(flyBy("xxxxxxxxx", "==T"));
+
+(function findUniq(arr: string[]) {
+  let array = arr.map((word) => {
+    let _arr = [...word];
+    _arr = _arr.filter((l) => l != " ").map((l) => l.toLowerCase());
+    return [...new Set(_arr)].sort();
+  });
+  for (let i = 0; i < array.length - 2; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (JSON.stringify(array[i]) != JSON.stringify(array[j])) {
+        return arr[j];
+      }
+    }
+  }
+})(["silvia", "vasili", "victor"]);
+</script>
+<template>
+  <h1>Kata 5</h1>
+</template>

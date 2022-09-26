343af9e4900bdce665b8d4ae9d01494c77858c1d
Levin_Ihor <lytghzys@gmail.com>
2022-09-26 21:51:19 +0300

    Day 13

---
+++ src/components/v-kata7.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// let number: any = 12233;
+// number = [...String(number)].map((number) => +number);
+// let res = 0;
+// for (let index = 1; index <= Math.max(...number); index++) {
+//   let count = number.filter((number: number) => number == index).length;
+//   index == count ? (res = count) : null;
+// }
+// console.log(res);
+
+// function sumPairs(ints: number[], s: number) {
+//   let res = { first: 0, second: 0, index: Infinity };
+//   let memo: any = [];
+//   for (let i = 0; i < ints.length - 1; i++) {
+//     if (memo.includes(ints[i])) {
+//       continue;
+//     }
+//     for (let j = i + 1; j < ints.length; j++) {
+//       if (ints[i] + ints[j] == s && j < res.index) {
+//         res = { first: ints[i], second: ints[j], index: j };
+//       }
+//     }
+//     memo.push(ints[i]);
+//   }
+//   return res.index == Infinity ? undefined : [res.first, res.second];
+// }
+
+let numb = 4
+
+for (let index = 0; index < numb; index++) {
+    
+}
+
+</script>
+<template><h1>Kata 7</h1></template>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import VKata3 from "../components/v-kata6.vue";
+import VKata3 from "../components/v-kata7.vue";
 </script>
 <template>
   <VKata3 />

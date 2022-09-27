541a3517d45a95e681d1e94cb3255a8ff81ffd27
Levin_Ihor <lytghzys@gmail.com>
2022-09-27 22:12:56 +0300

    Day 14

--- src/components/v-kata7.vue
+++ src/components/v-kata7.vue
@@ -25,11 +25,44 @@
 //   return res.index == Infinity ? undefined : [res.first, res.second];
 // }
 
-let numb = 4
+// class Test {
+//   x = 1;
+//   f() {
+//     console.log(this.x);
+//   }
+// }
+// let test = new Test();
+// console.log(test.f());
 
-for (let index = 0; index < numb; index++) {
-    
-}
+// function balancedNum(number: number): string {
+//   let length = number.toString().length;
+//   let res = "";
+//   if ((length & 1) === 0) {
+//     let left: any = number.toString().slice(0, length / 2 - 1);
+//     let right: any = number.toString().slice(-length / 2 + 1);
 
+//     console.log(left, right);
+//     left = [...left].reduce((acc, numb) => (acc += +numb), 0);
+//     right = [...right].reduce((acc, numb) => (acc += +numb), 0);
+//     res = left === right ? "Balanced" : "Not Balanced";
+//   } else {
+//     let left: any = number.toString().slice(0, Math.floor(length / 2));
+//     let right: any = number.toString().slice(-Math.ceil(length / 2) + 1);
+//     left = [...left].reduce((acc, numb) => (acc += +numb), 0);
+//     right = [...right].reduce((acc, numb) => (acc += +numb), 0);
+//     res = left === right ? "Balanced" : "Not Balanced";
+//   }
+//   return res;
+// }
+
+// console.log(balancedNum(56239814));
+
+// let str = "qwe0rt";
+// let length = str.length;
+// let res =
+//   (length & 1) === 0
+//     ? str.slice(Math.floor(length / 2) - 1, Math.ceil(length / 2) + 1)
+//     : str.slice(Math.floor(length / 2), Math.ceil(length / 2));
+// console.log(res);
 </script>
 <template><h1>Kata 7</h1></template>

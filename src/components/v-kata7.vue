42ee1460659efa8614beb7d77ddf98ded64dd8c5
Levin_Ihor <lytghzys@gmail.com>
2022-10-08 22:04:34 +0300

    Day 22

--- src/components/v-kata7.vue
+++ src/components/v-kata7.vue
@@ -251,5 +251,44 @@
 //     "0",
 //   ])
 // );
+
+function gematria(str: string): number {
+  let values: any = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    k: 10,
+    l: 20,
+    m: 30,
+    n: 40,
+    o: 50,
+    p: 60,
+    q: 70,
+    r: 80,
+    s: 90,
+    t: 100,
+    u: 200,
+    x: 300,
+    y: 400,
+    z: 500,
+    j: 600,
+    v: 700,
+    w: 900,
+  };
+  let sum = 0;
+  str = str.replace(/ /g, "");
+  for (let i = 0; i < str.length; i++) {
+    sum += values[str[i].toLowerCase()];
+    console.log(str[i]);
+  }
+  return sum;
+}
+console.log(gematria("Coding is fun"));
 </script>
 <template><h1>Kata 7</h1></template>
